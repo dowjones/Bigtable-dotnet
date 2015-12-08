0e3301edf804acd8413d235a2df2690c7a673ca5
Doug Swisher <doug.swisher@dowjones.com>
2015-12-08 10:37:38 -0600

    Add web example project

---
+++ src/Examples/Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Shared = BigtableNet.Common.ProductInfo;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle(Shared.Name + " Web Examples")]
+[assembly: AssemblyDescription(Shared.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(Shared.Company)]
+[assembly: AssemblyProduct(Shared.Name)]
+[assembly: AssemblyCopyright(Shared.Copyright)]
+[assembly: AssemblyTrademark(Shared.Trademark)]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion(Shared.Version)]
+[assembly: AssemblyFileVersion(Shared.FileVersion)]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("efdc4fad-9cc7-465a-a1db-c4cd17b9b4ef")]
